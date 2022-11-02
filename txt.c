266c6281e0fa88ba088b45d3406d1ad8b1c18918
DevOpsmkiran <mkiranmarigeri@gmail.com>
2022-11-02 08:52:17 +0530

    reverting back added code in line no 3 Revert "commiting for revert"
    This reverts commit 6229fffc26c6bb5469530e43990bae3b8b602fcf.

--- txt.c
+++ txt.c
@@ -1,6 +1,5 @@
 Entering code
 Fixing the laptop issue forrr push command
-revert back this line of code bcz already other developer has written the code
-next line of codes in the repo
 
+next line of codes in the repo
 
